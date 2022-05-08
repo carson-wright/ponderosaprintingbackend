8ab39b69235cf5041e751ade8f7899c0e99639f9
Carson Wright <carson@carsonwright.me>
2022-05-07 23:50:22 -0600

    Continued Work On Backend

--- src/api/delivery-method/routes/delivery-method.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * delivery-method router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::delivery-method.delivery-method');
--- src/api/delivery-method/services/delivery-method.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * delivery-method service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::delivery-method.delivery-method');

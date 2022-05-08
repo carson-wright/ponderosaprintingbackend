5babbd1d6080a37d310086bbce46355482100d06
Carson Wright <carson@carsonwright.me>
2022-05-08 00:15:51 -0600

    Fixed API token.salt

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
-  }
-  //apiToken: {
-    //salt: env('API_TOKEN_SALT'),
-  //},
+  },
+  apiToken: {
+    salt: crypto.randomBytes(16).toString('base64'),
+  },
 });
